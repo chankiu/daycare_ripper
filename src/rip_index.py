5d43c5395f95fddec7df6d48e417728dadf2545f
chankiu <chankiu@gmail.com>
2018-07-24 23:10:24 -0400

    request object still cannot handle ..

--- src/rip_index.py
+++ src/rip_index.py
@@ -7,6 +7,8 @@ import os
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 
+from urllib.parse import urlparse, urljoin
+
 import chankiu
 log = chankiu.log(sys.argv[0])
 
@@ -105,20 +107,21 @@ def get_details_from_index(index_url, log):
 if __name__ == "__main__":
     log.info("Ripping childcare data")
 
-    # log.info("Get All Index Links")
-    # all_index_links_az = []
-    # for index_link in get_az_links("https://www.toronto.ca/data/children/dmc/a2z/a2za.html", log):
-    #     all_index_links_az += get_az_links(index_link, log)
-    #
-    # all_detail_links = []
-    # for links in all_index_links_az:
-    #     all_detail_links += get_links_from_index(links, log)
-    #
-    # log.info("Get all detail links")
-    # for detail_links in all_detail_links:
-    #     print(detail_links)
+    log.info("Get All Index Links")
+    all_index_links_az = []
+    for index_link in get_az_links("https://www.toronto.ca/data/children/dmc/a2z/a2za.html", log):
+        all_index_links_az += get_az_links(index_link, log)
 
-    print(get_details("https://www.toronto.ca/data/children/dmc/a2z/../webreg/gcreg4186.html", log))
+    all_detail_links = []
+    for links in all_index_links_az:
+        all_detail_links += get_links_from_index(links, log)
+
+    log.info("Get all detail links")
+    for detail_links in all_detail_links:
+        print(detail_links)
+
+    print(len(all_detail_links))
+
 
 
 
